d98d13008c2597304a977de69dc7606434f6b689
muxriddin <muxriddin.developer@gmail.com>
2024-05-27 20:40:00 +0500

    source https ssl

--- src/MainUrl/index.js
+++ src/MainUrl/index.js
@@ -1,4 +1,4 @@
-export const mainUrl = "http://backend.lifesafety.uz/api/"
+export const mainUrl = "https://backend.lifesafety.uz/api/"
 // export const mainUrl = "https://lifesafety.pythonanywhere.com/api/"
 // export const mainUrl = "http://127.0.0.1:8000/api/";
 
